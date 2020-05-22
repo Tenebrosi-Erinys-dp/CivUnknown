bc7ec79f95ff43bdcd17d0ff3bde3d2b25e4b522
K-auiwfhwegjku <56566982+K-auiwfhwegjku@users.noreply.github.com>
2020-05-22 11:41:40 -0700

    Did some stuff in Melee
    a

--- Assets/Scripts/MeleeAttack.cs
+++ Assets/Scripts/MeleeAttack.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using Vector2 = System.Numerics.Vector2;
 
 public class Melee : MonoBehaviour
 {
     //box collider is trigger moves with player swing
-    //total time it takes for swing
     //rotate swing box collider
     //Input class fire1 Input.Getbutton("Fire1");
-    //variable for swing range (box collider size)
 
-
+    GameObject Swing = GameObject.Find("Swing");
+    public double swingTime = 0;
+    public Vector2 BoxColliderSize = Vector2.Zero;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,4 +24,8 @@ public class Melee : MonoBehaviour
     {
         
     }
+    //Damage enemies hit by swing (collider)
+    private void OnTriggerEnter2D(Collider2D collider) { 
+    
+    } 
 }
