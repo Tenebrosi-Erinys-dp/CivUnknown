bcf9ea5cb599897d552233a8122595199c65cfbd
Tenebrosi-Erinys <nickerb40@gmail.com>
2020-05-27 17:25:49 -0700

    Improved melee enemy AI

--- Assets/Scripts/MeleeEnemy.cs
+++ Assets/Scripts/MeleeEnemy.cs
@@ -8,9 +8,9 @@ public class MeleeEnemy : EnemyController
     Hitbox hitboxInstance;
     public float attackRange = 2f;
     public float attackRadius = 45;
-    public float attackWindup = 0.5f;
     public float attackDuration = 1f;
     public float attackRecovery = 0.5f;
+    public float runAwayRange = 10f;
     bool attacking = false;
     // Start is called before the first frame update
     new protected void Start()
@@ -33,40 +33,60 @@ public class MeleeEnemy : EnemyController
             //IF player is too close AND cooldown is off, attack
             if(!attacking && distance < attackRange && attackCD <= 0)
             {
+                attackCD = maxAttackCD;
                 attacking = true;
                 StartCoroutine(MeleeAttack());
             }
-            else if (attacking)
+            //If player is too far AND cooldown is off, attack
+            else if (!attacking && distance >= attackRange && attackCD <= 0.5f)
             {
-                //Do nothing else
+                //Go closer
+                Debug.Log("Closing In");
+                MoveInDirection(player - (Vector2)transform.position);
+                rb.MoveRotation(Vector2Angle(player));
             }
-            //Otherwise, run away
-            else
+            //If the cooldown is too long and player is too close, run
+            else if(!attacking && distance < runAwayRange)
             {
                 Vector2 direction = FlipAroundSelf(player);
                 direction -= (Vector2)transform.position;
 
-
                 //ADD BACK AFTER TESTING
                 MoveInDirection(direction);
-                rb.MoveRotation(Vector2Angle(FlipAroundSelf(player)));
+                rb.MoveRotation(Vector2Angle(player));
             }
+            //Otherwise, wait
+        }
+    }
+
+    IEnumerator RotateTo(Vector2 position, float seconds)
+    {
+        float timer = 0;
+        float initialRot = rb.rotation;
+        float newRot = Vector2Angle(position);
+        while(timer < seconds)
+        {
+            timer += Time.deltaTime;
+            rb.MoveRotation(Mathf.Lerp(initialRot, newRot, timer / seconds));
+            yield return null;
+        }
+    }
+
+    IEnumerator RotateToClamped(Vector2 position, float maxDegreesPerSecond)
+    {
+        float initialRot = rb.rotation;
+        float newRot = Vector2Angle(position);
+        while (Mathf.Approximately(rb.rotation, newRot))
+        {
+            rb.MoveRotation(rb.rotation + maxDegreesPerSecond);
+            yield return null;
         }
     }
 
     IEnumerator MeleeAttack()
     {
-        //Rotate towards player
-        float timer = 0f;
-        while(timer < attackWindup)
-        {
-            timer += Time.deltaTime;
-            rb.MoveRotation(Mathf.Lerp(rb.rotation, Mathf.Rad2Deg * Vector2Angle(player), timer / attackWindup));
-            yield return null;
-        }
-
         //Generate attack hitbox
-        timer = 0f;
+        float timer = 0f;
         float rotation = rb.rotation - attackRadius / 2f;
         float finalRotation = rb.rotation + attackRadius / 2f;
         hitboxInstance = Instantiate(hitbox);
