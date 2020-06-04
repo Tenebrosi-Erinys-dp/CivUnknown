0ae5fa46774d3f52c0b7e7db930e49e5b2dbbbcc
K-auiwfhwegjku <56566982+K-auiwfhwegjku@users.noreply.github.com>
2020-06-04 15:21:56 -0700

    what eveb

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManager;
 
 public class StartGame : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObject.onClick.
     }
 
     // Update is called once per frame
