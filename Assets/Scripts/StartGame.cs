06f05e3e44aeb93016819530ea7047bc91ce2cb8
K-auiwfhwegjku <56566982+K-auiwfhwegjku@users.noreply.github.com>
2020-06-04 15:56:21 -0700

    Butotn works
    Yeeeeee

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -1,20 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManager;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartGame : MonoBehaviour
 {
-    
+    Button button;
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.onClick.
+        button = GameObject.Find("Button").AddComponent<Button>();
+        button.onClick.AddListener(ChangeScene);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void ChangeScene() {
+        SceneManager.LoadScene("Nick's Sandbox", LoadSceneMode.Single);
     }
 }
