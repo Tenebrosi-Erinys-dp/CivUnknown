41ca6a23a019a114d7b80e12d437217ac6009814
Tenebrosi-Erinys <nickerb40@gmail.com>
2020-06-04 14:08:39 -0700

    Added game won scene, plays once all enemies are dead.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    public static List<EnemyController> enemies;
 
     private void Start()
     {
         instance = this;
+        enemies = FindObjectsOfType<EnemyController>().ToList<EnemyController>();
     }
+
+    public static void CheckGameWin()
+    {
+        bool isAllEmpty = true;
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            if(enemies[i] != null)
+            {
+                isAllEmpty = false;
+            }
+        }
+
+        if (isAllEmpty)
+        {
+            SceneManager.LoadSceneAsync("Game Won");
+        }
+    }
+
+
     public void GameOver()
     {
         SceneManager.LoadSceneAsync("Game Over");
