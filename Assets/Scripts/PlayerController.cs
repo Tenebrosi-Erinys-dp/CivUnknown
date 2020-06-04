73e7278a481393df33f29890ead44713a50ca5a1
Tenebrosi-Erinys <nickerb40@gmail.com>
2020-06-04 15:19:42 -0700

    Attempting to fix issue

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -53,10 +53,12 @@ public class PlayerController : EntityController
     public float dingTimer = 4;
 
     SpriteRenderer sr;
+    LayerMask wall;
 
     // Start is called before the first frame update
     new void Start()
     {
+        wall = LayerMask.GetMask("Wall");
         self = "Mummy";
         myAnim = GetComponent<Animator>();
         head = GameObject.Find("HeadObject");
@@ -189,8 +191,9 @@ public class PlayerController : EntityController
         laserAudio.Play();
         while(timer < spellDuration)
         {
+            RaycastHit2D hit = Physics2D.Raycast(head.transform.position, head.transform.forward, Mathf.Infinity, wall);
             timer += Time.deltaTime;
-            hitboxInstance.transform.localScale = new Vector3(spellRange, Mathf.Lerp(0, spellWidth, timer / spellDuration), 1);
+            hitboxInstance.transform.localScale = new Vector3(Mathf.Min(spellRange, hit.distance), Mathf.Lerp(0, spellWidth, timer / spellDuration), 1);
             hitboxInstance.transform.rotation = head.transform.rotation;
             hitboxInstance.transform.eulerAngles = new Vector3(0, 0, hitboxInstance.transform.eulerAngles.z + 90);
             hitboxInstance.transform.position = head.transform.position;
